aa84dcad5266cf6e1f4020449f0f657fa9419ec3
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2026-01-02 15:53:48 +0800

    Feat/json support (#4)
    * feat: added json support
    
    * feat: added serde dependencies

--- src/log.rs
+++ src/log.rs
@@ -1,9 +1,11 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+use serde::Serialize;
+
+#[derive(Debug, Serialize, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum LogLevel {
     Info,
     Warn,
     Error,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct LogEntry(pub LogLevel, pub String);
